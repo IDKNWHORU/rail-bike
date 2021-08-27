46d897e6abf06aa052b66a3f7033d27bbb055f7c
junho9341 <junho9341@gmail.com>
2021-08-27 15:58:47 +0900

    UPDATE : 반복문으로 처리하도록 변경, 체이닝 함수 가독성 최적화

--- service/src/main/resources/static/js/biz/Item.js
+++ service/src/main/resources/static/js/biz/Item.js
@@ -1,10 +1,5 @@
-const addItemRow = () => {
-    const cloneRowNode = myRowTag.cloneNode(true);
-
-    Array.from(myRowTag.cloneNode(true)).forEach(node=>(node.children[0].type !== 'button') ? node.children[0].value = '' : false);
-
-    document.querySelector('#item_table>tbody').appendChild(cloneRowNode);
-}
+const addItemRow = () => document.querySelector('#item_table>tbody')
+                                 .appendChild(myRowTag.cloneNode(true));
 
 const removeDuplicatedRecord = () => Array.from(document.querySelectorAll('#item_rows'))
                                           .slice(1)
@@ -14,24 +9,7 @@ const searchItemList = (itemList = []) => {
     removeDuplicatedRecord();
     itemList.forEach((itemMap)=>{
         addItemRow();
-        
-        const rowTags = document.querySelectorAll('#item_rows');
-        const rowTag = rowTags[rowTags.length-1];
-
-        const item_code = rowTag.querySelector('#item_code');
-        const item_name = rowTag.querySelector('#item_name');
-        const unit_name = rowTag.querySelector('#unit_name');
-        const price = rowTag.querySelector('#price');
-        const order = rowTag.querySelector('#order');
-
-        item_code.value = itemMap.item_code;
-        item_name.value = itemMap.item_name;
-        unit_name.value = itemMap.unit_name;
-        price.value = itemMap.price;
-        order.value = itemMap.order;
-
-        item_code.readOnly = true;
-
-        item_code.classList.add('input-readOnly')
+        Array.from(document.querySelectorAll('#item_rows:last-child>td')).forEach(column => (column.children[0].type !== 'button') ? column.children[0].value = itemMap[column.children[0].id] : '');
     });
+    Array.from(document.querySelectorAll('#item_code')).slice(1).forEach(node => node.readOnly = true);
 }
