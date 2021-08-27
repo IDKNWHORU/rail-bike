8841d2f1576a88d81ef8f08507aa6363242cb925
junho9341 <junho9341@gmail.com>
2021-08-27 16:51:35 +0900

    UPDATE : 코드 간결화

--- service/src/main/resources/static/js/biz/Item.js
+++ service/src/main/resources/static/js/biz/Item.js
@@ -1,15 +1,7 @@
-const addItemRow = () => document.querySelector('#item_table>tbody')
-                                 .appendChild(myRowTag.cloneNode(true));
+const addItemRow = (tableBody, rowNode)  => tableBody.appendChild(rowNode);
 
-const removeDuplicatedRecord = () => Array.from(document.querySelectorAll('#item_rows'))
-                                          .slice(1)
-                                          .forEach(row => row.parentElement.removeChild(row));
+const removeDuplicatedRecord = (parentElement, row) => parentElement.removeChild(row);
 
-const searchItemList = (itemList = []) => {
-    removeDuplicatedRecord();
-    itemList.forEach((itemMap)=>{
-        addItemRow();
-        Array.from(document.querySelectorAll('#item_rows:last-child>td')).forEach(column => (column.children[0].type !== 'button') ? column.children[0].value = itemMap[column.children[0].id] : '');
-    });
-    Array.from(document.querySelectorAll('#item_code')).slice(1).forEach(node => node.readOnly = true);
-}
+const searchItemList = (inputTag, itemMap, role) => inputTag.value = itemMap[role];
+
+const changeAttributeReadOnly = itemCode => itemCode.readOnly = true;
