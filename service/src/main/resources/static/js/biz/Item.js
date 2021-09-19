711060f94e096a4ef3d061e7fe070d422597348d
junhojava <junho9341@gmail.com>
2021-09-20 03:24:28 +0900

    UPDATE : 함수 이동

--- service/src/main/resources/static/js/biz/Item.js
+++ service/src/main/resources/static/js/biz/Item.js
@@ -1,7 +1,43 @@
 const addItemRow = (tableBody, rowNode)  => tableBody.appendChild(rowNode);
 
-const removeDuplicatedRecord = (parentElement, row) => parentElement.removeChild(row);
+const appendDataListOption = (unitMap, dataList, option) => {
+    option.value = unitMap.code;
+    option.label = unitMap.name;
 
-const searchItemList = (inputTag, itemMap, role) => inputTag.value = itemMap[role];
+    dataList.appendChild(option);
+};
 
 const changeAttributeReadOnly = itemCode => itemCode.readOnly = true;
+
+const getFieldsSelector = prefix => {
+    return {
+        textFieldSelector : `${prefix}input[type="text"]`,
+        numberFieldSelector : `${prefix}input[type="number"]`,
+        unitFieldSelector : `${prefix}${unitNameSelector}`
+    }
+}
+
+const getItemInfo = () => makeFetch(url, {method: 'GET'}, search);
+
+const getUnitInfo = (userUrl) => makeFetch(userUrl, { method: 'GET' }, putUnitOptions);
+
+const putUnitOptions = unitList => {
+    const dataList = document.querySelector('datalist');
+    const option = document.querySelector('datalist>option').cloneNode(true);
+
+    unitList.forEach(unitMap => appendDataListOption(unitMap, dataList, option));
+}
+
+const removeItemList = itemRows => {itemRows.forEach(removeRows)};
+
+const removeRows = (row) => {
+    const parentElement = row.parentElement;
+
+    parentElement.removeChild(row)
+}
+
+const searchItemList = (inputTag, itemMap) => {
+    const role = inputTag.getAttribute('role');
+
+    inputTag.value = itemMap[role];
+}
