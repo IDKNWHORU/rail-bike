d7d33c590d946d2b20fcd1cf0bb9829ec1649d67
junhojava <junho9341@gmail.com>
2021-09-20 03:23:58 +0900

    UPDATE : fetch 함수의 validation

--- service/src/main/resources/static/js/ajax/Ajax.js
+++ service/src/main/resources/static/js/ajax/Ajax.js
@@ -34,8 +34,22 @@ const responseCallBack = () => {
     }
 };
 
-const makeFetch = (userUrl, userMethod, callbackFunction) => {
-    fetch(userUrl, { method: userMethod })
-        .then((res) => res.json())
-        .then((data) => callbackFunction(data));
+const makeFetch = (userUrl = '', optionInfo = {method: 'GET'}, callbackFunction = (data) => { return ;}) => {
+    return fetch(userUrl, validationOptionInfo(optionInfo))
+            .then((res) => res.json())
+            .then(callbackFunction);
 };
+
+const validationOptionInfo = (optionInfo) => {
+    return (optionInfo.method === 'GET') ? removeBodyOption(optionInfo) : resumeOption(optionInfo);
+}
+
+const removeBodyOption = optionInfo => {
+    optionInfo.body = undefined;
+
+    return optionInfo;
+}
+
+const resumeOption = optionInfo => {
+    return optionInfo;
+}
