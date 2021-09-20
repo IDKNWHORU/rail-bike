b69e73998bc6b290adc6677b97ba2dc6aea89962
junhojava <junho9341@gmail.com>
2021-09-21 00:26:44 +0900

    UPDATE : 더이상 사용하지 않는 함수 제거

--- service/src/main/resources/static/js/ajax/Ajax.js
+++ service/src/main/resources/static/js/ajax/Ajax.js
@@ -1,39 +1,3 @@
-let callBack;
-let httpRequest;
-
-const makeRequest = (callBackFunction, method, url, requestHeader, queryString) => {
-    httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-        console.error(`Can't make XMLHTTP instance.`);
-        return false;
-    }
-
-    callBack = callBackFunction;
-    httpRequest.onreadystatechange = responseCallBack;
-    httpRequest.open(method, url);
-
-    if (requestHeader !== null) {
-        httpRequest.setRequestHeader('Content-Type', requestHeader);
-    }
-
-    httpRequest.send(queryString);
-};
-
-const responseCallBack = () => {
-    try {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-            if (httpRequest.status === 200) {
-                callBack(httpRequest);
-            } else {
-                console.error('request has has problem..');
-            }
-        }
-    } catch (error) {
-        console.error(`Request Exception : ${error.message}`);
-    }
-};
-
 const makeFetch = (userUrl = '', optionInfo = {method: 'GET'}, callbackFunction = (data) => { return ;}) => {
     return fetch(userUrl, validationOptionInfo(optionInfo))
             .then((res) => res.json())
