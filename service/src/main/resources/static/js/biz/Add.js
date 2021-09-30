42904c7e2064b8e74d04b2f94afca30b8ff4cb42
junhojava <junho9341@gmail.com>
2021-09-30 18:56:09 +0900

    NEW : 스크립트 파일 분리

---
+++ service/src/main/resources/static/js/biz/Add.js
@@ -0,0 +1,26 @@
+const copyRecord = node => {
+    return node.cloneNode(true);
+}
+
+const putOrderColumn = (column, orderObject) => {
+    const objectKey = column.dataset.role;
+
+    column.innerText = orderObject[objectKey];
+}
+
+const putOrderInfo = (orderObject, orderTable, orderNode) => {
+    const orderRecord = copyRecord(orderNode);
+    const orderColumns = Array.from(orderRecord.children);
+
+    orderColumns.forEach(column=> putOrderColumn(column, orderObject));
+
+    orderTable.appendChild(orderRecord);
+}
+
+const putOrderList = (orderList) => {
+    const rowSelector = ".order-row";
+    const tableTag = document.querySelector('table');
+    const orderNode = document.querySelector(rowSelector);
+
+    orderList.forEach(orderObject => putOrderInfo(orderObject, tableTag, orderNode));
+}
