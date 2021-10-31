3469909e11918211d8bd2759e65cc08d08206fe5
junhojava <junho9341@gmail.com>
2021-10-31 17:08:53 +0900

    UPDATE : 품목 하이퍼링크 선택시 화면 이동

---
+++ service/src/main/resources/static/js/biz/Main.js
@@ -0,0 +1,19 @@
+const clickNav = (selection, queryStr = '') => {
+  const dom = selection.ownerDocument;
+  const direction = selection.dataset.nav;
+  const beforeSeleted = dom.querySelector('.active');
+
+  changeActive(beforeSeleted, selection);
+  callFile(direction, queryStr, dom);
+};
+
+const callFile = (direction, queryStr, dom) => {
+  const embedTag = dom.querySelector('embed');
+
+  embedTag.setAttribute('src', `${direction}.html?${queryStr}`);
+};
+
+const changeActive = (before, selected) => {
+  before.classList.remove('active');
+  selected.classList.add('active');
+};
