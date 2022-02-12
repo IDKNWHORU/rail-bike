91f91d675fb1672cd2fb80091ba216a407c3524d
junho <j1u2n3h4o7>
2022-02-12 14:29:19 +0000

    new fetch function "apiCall"

--- service/src/main/resources/static/js/ajax/Ajax.js
+++ service/src/main/resources/static/js/ajax/Ajax.js
@@ -1,19 +1,54 @@
-const makeFetch = (userUrl = '', optionInfo = {method: 'GET'}, callbackFunction = (data) => { return ;}) => {
-    return fetch(userUrl, validationOptionInfo(optionInfo))
-            .then((res) => res.json())
-            .then(callbackFunction);
+const makeFetch = (
+  userUrl = "",
+  optionInfo = { method: "GET" },
+  callbackFunction = (data) => {
+    return;
+  }
+) => {
+  return fetch(userUrl, validationOptionInfo(optionInfo))
+    .then((res) => res.json())
+    .then(callbackFunction);
 };
 
 const validationOptionInfo = (optionInfo) => {
-    return (optionInfo.method === 'GET') ? removeBodyOption(optionInfo) : resumeOption(optionInfo);
+  return optionInfo.method === "GET"
+    ? removeBodyOption(optionInfo)
+    : resumeOption(optionInfo);
 };
 
-const removeBodyOption = optionInfo => {
-    optionInfo.body = undefined;
+const removeBodyOption = (optionInfo) => {
+  optionInfo.body = undefined;
 
-    return optionInfo;
+  return optionInfo;
 };
 
-const resumeOption = optionInfo => {
-    return optionInfo;
+const resumeOption = (optionInfo) => {
+  return optionInfo;
+};
+
+const defaultCB = (data) => {
+  return "define your callback function";
+};
+
+const apiCall = {
+  #method: {
+      get: {
+          method: "GET"
+      },
+      post: {
+          method: "POST"
+      },
+      put: {
+          method: "PUT"
+      },
+      delete: {
+          method: "DELETE"
+      }
+  },
+  get: async (url = "/", callback = defaultCB) => {
+      const serverResponse = await fetch(url, _method.get, callback);
+      const jsonData = serverResponse.json();
+
+      return callback(jsonData);
+  },
 };
