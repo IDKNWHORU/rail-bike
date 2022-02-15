60599f30996148c8646c98fe485843a6a96e74b2
j1u2n3h4o7 <j1u2n3h4o7>
2022-02-15 09:40:30 +0900

    add date field component

---
+++ service/src/main/resources/static/cra/src/component/DateField.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const DateField = () => {
+  return (
+    <div>
+      <div>
+        <span>입력일자</span>
+      </div>
+      <div>
+        <input type="date" required></input>
+      </div>
+    </div>
+  );
+};
+
+export default DateField;
