13ffccfc604cd0ef9b238668081cb5e2dafe9662
j1u2n3h4o7 <j1u2n3h4o7>
2022-02-15 10:38:16 +0900

    add grid record component

---
+++ service/src/main/resources/static/cra/src/component/GridRecord.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import GridColumn from "./GridColumn";
+
+const GridRecord = () => {
+  return (
+    <div>
+      <GridColumn text="1"></GridColumn>
+      <GridColumn text="20210212"></GridColumn>
+      <GridColumn text="1234"></GridColumn>
+      <GridColumn text="노주브타이어"></GridColumn>
+      <GridColumn text="100"></GridColumn>
+      <GridColumn text="500,000"></GridColumn>
+      <GridColumn text="100,000"></GridColumn>
+      <GridColumn text="600,000"></GridColumn>
+    </div>
+  );
+};
+
+export default GridRecord;
