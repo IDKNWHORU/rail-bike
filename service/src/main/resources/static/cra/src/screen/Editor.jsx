10316d89b7564c6d6a92412473b93f84e54d708a
j1u2n3h4o7 <j1u2n3h4o7>
2022-02-15 13:32:00 +0900

    add editor screen

---
+++ service/src/main/resources/static/cra/src/screen/Editor.jsx
@@ -0,0 +1,13 @@
+import Content from "../component/Content";
+import SideBar from "../component/SideBar";
+
+const Editor = () => {
+  return (
+    <>
+      <SideBar title="전표 수정 폼"></SideBar>
+      <Content title="전표 목록"></Content>
+    </>
+  );
+};
+
+export default Editor;
