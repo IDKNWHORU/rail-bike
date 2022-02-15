c4abc37ec7e99e86510516b0af674b9070c5c544
j1u2n3h4o7 <j1u2n3h4o7>
2022-02-15 10:19:48 +0900

    add side bar component

---
+++ service/src/main/resources/static/cra/src/component/SideBar.jsx
@@ -0,0 +1,14 @@
+import InputForm from "./InputForm";
+
+const SideBar = () => {
+  return (
+    <div>
+      <h1>
+        <span>전표 입력 폼</span>
+      </h1>
+      <InputForm></InputForm>
+    </div>
+  );
+};
+
+export default SideBar;
