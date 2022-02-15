01ca31a4a95cde8a8166f8b23dd3728214642d3e
j1u2n3h4o7 <j1u2n3h4o7>
2022-02-15 09:46:34 +0900

    add datalist field component

---
+++ service/src/main/resources/static/cra/src/component/DataList.jsx
@@ -0,0 +1,14 @@
+const DataList = () => {
+  return (
+    <div>
+      <div>
+        <span>품목</span>
+      </div>
+      <div>
+        <input type="datalist" placeholder="품목을 선택해주세요" required />
+      </div>
+    </div>
+  );
+};
+
+export default DataList;
