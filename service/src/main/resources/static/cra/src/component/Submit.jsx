f06e5cbad09d5c6b458562d8f103dce2cf208689
j1u2n3h4o7 <j1u2n3h4o7>
2022-02-15 09:40:14 +0900

    add submit btn

---
+++ service/src/main/resources/static/cra/src/component/Submit.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Submit = () => {
+  return (
+    <div>
+      <div>
+        <input type="submit" value="입력하기" />
+      </div>
+    </div>
+  );
+};
+
+export default Submit;
