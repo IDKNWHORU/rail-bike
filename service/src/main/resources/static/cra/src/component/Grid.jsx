36a395d188e3cdf0e6b11ac69c3c1bce1b5ac9f4
j1u2n3h4o7 <j1u2n3h4o7>
2022-02-15 10:35:20 +0900

    add grid component

---
+++ service/src/main/resources/static/cra/src/component/Grid.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import GridHeader from "./GridHeader";
+
+const Grid = () => {
+  return (
+    <div>
+      <GridHeader></GridHeader>
+      <div>
+        <div>1</div>
+        <div>20210212</div>
+        <div>1234</div>
+        <div>노주브타이어</div>
+        <div>100</div>
+        <div>500,000</div>
+        <div>100,000</div>
+        <div>600,000</div>
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
